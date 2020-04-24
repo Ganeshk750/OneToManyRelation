66c7d53cea6402d5371c41adfb78db80589bbd6b
Ganeshk750 <ganesh9ot@gmail.com>
2020-04-24 23:09:52 +0530

    adding repositroy and establish relation

--- Spring-BlogApp-1/src/main/java/com/ganesh/model/Address.java
+++ Spring-BlogApp-1/src/main/java/com/ganesh/model/Address.java
@@ -1,13 +1,48 @@
 package com.ganesh.model;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
-public class Address {
+public class Address extends AbstractPersistable<Long>{
 	
 	private String state;
 	private String city;
 	private String pin;
+	
+	@ManyToOne
+	@JoinColumn(name="student_id")
+	private Student student;
+	
+	
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
 	
 
 }
--- Spring-BlogApp-1/src/main/java/com/ganesh/model/Student.java
+++ Spring-BlogApp-1/src/main/java/com/ganesh/model/Student.java
@@ -1,5 +1,59 @@
 package com.ganesh.model;
 
-public class Student {
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+public class Student extends AbstractPersistable<Long> {
+	
+  private String studentId;
+  private String name;
+  private String branch;
+  private String email;
+  
+  @OneToMany(targetEntity=Address.class, mappedBy="student", fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+  private Set<Address> addresses;
+  
+public String getStudentId() {
+	return studentId;
+}
+public void setStudentId(String studentId) {
+	this.studentId = studentId;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getBranch() {
+	return branch;
+}
+public void setBranch(String branch) {
+	this.branch = branch;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public Set<Address> getAddresses() {
+	return addresses;
+}
+public void setAddresses(Set<Address> addresses) {
+	this.addresses = addresses;
+}
+  
+  
+
+  
+  
+  
 }
