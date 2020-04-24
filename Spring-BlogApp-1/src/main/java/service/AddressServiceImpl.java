c3c0f9b248f80b9e52c4868d15799ce966232440
Ganeshk750 <ganesh9ot@gmail.com>
2020-04-24 23:23:13 +0530

    adding serviceImpl class and Implementing Methods

---
+++ Spring-BlogApp-1/src/main/java/service/AddressServiceImpl.java
@@ -0,0 +1,33 @@
+package service;
+
+import java.util.List;
+
+import com.ganesh.model.Address;
+
+public class AddressServiceImpl implements AddressService {
+
+	@Override
+	public List<Address> addressList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address addAddress(Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteAddress(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Spring-BlogApp-1/src/main/java/service/StudentServiceImpl.java
@@ -0,0 +1,33 @@
+package service;
+
+import java.util.List;
+
+import com.ganesh.model.Student;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public List<Student> studentList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student findOne(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteStudent(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
